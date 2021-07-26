30f5e4c6205f04ae0d6fceffe5b1b635fb2437ca
jonghopark95 <jong951005@gmail.com>
2021-07-27 08:43:25 +0900

    build : Babel 설치 및 option 들 추가

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,6 +6,22 @@ const config = {
     path: path.resolve(__dirname, './dist'),
     filename: 'build.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /mode_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: '> 0.25%, not dead', useBuiltIns: 'usage', corejs: 3, modules: false }],
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
 
 module.exports = config;
