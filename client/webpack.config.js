78b72a9d2476526b4801b06f183332a3e0fe6b30
jonghopark95 <jong951005@gmail.com>
2021-07-27 09:15:01 +0900

    build : url-loader, file-loader, html-webpack-plugin 추가

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
@@ -27,11 +28,28 @@ const config = {
           'sass-loader', //  Sass를 CSS로 Compile
         ],
       },
+      {
+        test: /\.(png|jpe?g|gif|svg|ico)$/,
+        use: [
+          {
+            loader: 'url-loader', //  파일 관리를 위한 loader, file-loader 대체
+            options: {
+              name: 'images/[name].[ext]?[hash]',
+              limit: 10000, //  작은 파일이나 글꼴은 파일 복사가 아닌 문자열로 변환. bundle 파일이 가벼워짐
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'style.css',
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'), //  어느 파일을 참조할 것인가
+      inject: true, //  번들링한 파일을 자동으로 불러올 것인가
+      filename: path.resolve(__dirname, './dist/index.html'),
     }),
   ],
 };
