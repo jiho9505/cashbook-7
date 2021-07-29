591807d2ef4d1a3cb7c9acf11069e81b85197743
jonghopark95 <jong951005@gmail.com>
2021-07-29 23:56:19 +0900

    feat : Container 만들어주는 유틸함수 추가

--- client/src/utils/helper.ts
+++ client/src/utils/helper.ts
@@ -4,3 +4,24 @@ export const $ = (
 ): Element => {
   return parentNode.querySelector(selector);
 };
+
+export const Container = ({
+  tagName = 'div', //
+  id = undefined,
+  className = undefined,
+  child = undefined,
+}) => {
+  const Node = document.createElement(tagName);
+  if (id) Node.setAttribute('id', id);
+  if (className) Node.setAttribute('class', className);
+  Node.innerHTML = getChildHTML(child);
+
+  return Node.outerHTML;
+};
+
+const getChildHTML = (child: string | string[]) => {
+  if (!child) return '';
+  if (!Array.isArray(child)) return child;
+
+  return child.reduce((acc, curr) => acc + getChildHTML(curr), '');
+};
