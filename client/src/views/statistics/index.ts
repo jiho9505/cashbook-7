f27b263fa5cfbf3186d686beb63da9710fc32b26
jonghopark95 <jong951005@gmail.com>
2021-07-29 23:56:43 +0900

    feat : statistic에서 사용할 wrapper container 세팅

--- client/src/views/statistics/index.ts
+++ client/src/views/statistics/index.ts
@@ -1,5 +1,7 @@
+import './index.scss';
 import handleEvent from '@src/utils/handleEvent';
-import { $ } from '@src/utils/helper';
+import { $, Container } from '@src/utils/helper';
+// import DoughnutChart from './DoughnutChart/DoughnutChart';
 
 export default class StatisticsView {
   constructor() {
@@ -11,6 +13,61 @@ export default class StatisticsView {
   }
 
   render() {
-    $('.content-wrap').innerHTML = `<h1>Statistics!!!</h1>`;
+    $('.content-wrap').innerHTML = `
+      <div class='content__statistics'>
+        ${this.DoughnutChart()}
+        ${this.RecentlyAccountHistory()}
+        ${this.CurvedChart()}
+      </div>
+    `;
   }
+
+  // Component
+  DoughnutChart() {
+    return Container({
+      tagName: 'div',
+      className: 'doughnut-chart',
+      child: [this.DoughnutChartTitle()],
+    });
+  }
+
+  DoughnutChartTitle = () => {
+    return Container({
+      tagName: 'span',
+      className: 'doughnut-chart__title',
+      child: '카테고리 별 지출',
+    });
+  };
+
+  RecentlyAccountHistory() {
+    return Container({
+      tagName: 'div',
+      className: 'recently-account-history',
+      child: [this.RecentlyAccountHistoryTitle()],
+    });
+  }
+
+  RecentlyAccountHistoryTitle = () => {
+    return Container({
+      tagName: 'span',
+      className: 'recently-account-history__title',
+      child: '최근 가계부 내역',
+    });
+  };
+
+  CurvedChart() {
+    return Container({
+      tagName: 'div',
+      className: 'curved-chart',
+      child: [this.CurvedChartTitle()],
+    });
+  }
+
+  CurvedChartTitle = () => {
+    return Container({
+      tagName: 'span',
+      className: 'curved-chart__title',
+      child: '생활 카테고리 소비 추이',
+    });
+  };
 }
