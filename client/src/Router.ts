3cb88354e80a5226ea0477705b79e7813a7cdfe0
jonghopark95 <jong951005@gmail.com>
2021-07-29 17:35:54 +0900

    chore : console.log 지우기

--- client/src/Router.ts
+++ client/src/Router.ts
@@ -19,7 +19,6 @@ export default class Router {
     });
 
     handleEvent.subscribe('statechange', (e: CustomEvent) => {
-      console.log(e);
       this.stateChangeHandler({ state: e.detail });
     });
   }
@@ -29,7 +28,6 @@ export default class Router {
       delete e.state.isReplace;
       history.replaceState(e.state, '', e.state.path as string);
     } else {
-      console.log('push');
       history.pushState(e.state, '', e.state.path as string);
     }
   }
