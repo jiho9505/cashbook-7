6c0b7825a2a0d45489a74066ec8a7d7a7fca747b
jonghopark95 <jong951005@gmail.com>
2021-07-29 11:49:32 +0900

    chore : images 추가

---
+++ client/src/static/imageUrls.ts
@@ -0,0 +1,7 @@
+const BASE_URL = 'https://cashbook-7.s3.ap-northeast-2.amazonaws.com/';
+
+export const Logo = BASE_URL + 'logo.svg';
+export const Account = BASE_URL + 'account.svg';
+export const Statistic = BASE_URL + 'statistic.svg';
+export const Calendar = BASE_URL + 'calendar.svg';
+export const Logout = BASE_URL + 'logout.svg';
