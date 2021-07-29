730a0f1a7eb8af8af25b62db44dd16fbc8cf9045
jonghopark95 <jong951005@gmail.com>
2021-07-30 01:54:25 +0900

    feat : recently account component 로직 및 스타일 코드 추가

--- client/src/views/statistics/index.ts
+++ client/src/views/statistics/index.ts
@@ -1,7 +1,9 @@
 import './index.scss';
 import handleEvent from '@src/utils/handleEvent';
-import { $, Container } from '@src/utils/helper';
-// import DoughnutChart from './DoughnutChart/DoughnutChart';
+import { $, changeIntoDateFormat, Container } from '@src/utils/helper';
+import { dummyData } from '@src/dummyData';
+import { AccountData } from '@src/types';
+import { PICTOGRAM } from '@src/static/statistics';
 
 export default class StatisticsView {
   constructor() {
@@ -9,7 +11,18 @@ export default class StatisticsView {
       if (e.detail.state.path !== '/statistics') return;
 
       this.render();
+      $('.recently-account-history__detail').addEventListener('click', this.onClickDetailAccount);
     });
+  }
+  recentlyAccountData = dummyData.sort((p, n) => n.date - p.date).splice(0, 3);
+
+  onClickDetailAccount(e: MouseEvent) {
+    e.preventDefault();
+
+    const { target } = e;
+    if (!(target instanceof HTMLElement)) return;
+    const path = target.getAttribute('href');
+    handleEvent.fire('statechange', { ...history.state, path });
   }
 
   render() {
@@ -43,7 +56,11 @@ export default class StatisticsView {
     return Container({
       tagName: 'div',
       className: 'recently-account-history',
-      child: [this.RecentlyAccountHistoryTitle()],
+      child: [
+        this.RecentlyAccountHistoryTitle(),
+        this.AccountDetailTag(),
+        [...this.recentlyAccountData.map((d) => this.RecentlyAccount(d))],
+      ],
     });
   }
 
@@ -53,6 +70,12 @@ export default class StatisticsView {
       className: 'recently-account-history__title',
       child: '최근 가계부 내역',
     });
+  };
+
+  AccountDetailTag = () => {
+    return `
+      <a href='/account' class='recently-account-history__detail'>자세히 보기 ></a>
+    `;
   };
 
   CurvedChart() {
@@ -68,6 +91,28 @@ export default class StatisticsView {
       tagName: 'span',
       className: 'curved-chart__title',
       child: '생활 카테고리 소비 추이',
+    });
+  };
+
+  RecentlyAccount = (data: AccountData) => {
+    const splitedMoney = data.amountOfMoney.split(',');
+
+    return Container({
+      tagName: 'div',
+      className: 'recently-account',
+      child: `
+        <img src=${PICTOGRAM[data.category]}>
+        <div>
+          <span class='recently-account__title'>${data.title}</span>
+          <span class='recently-account__date'>${changeIntoDateFormat(data.date)}</span>
+        </div>
+        <span class='recently-account__date'>
+          ${!data.isIncome ? '-₩' : '₩'}
+          ${splitedMoney.splice(0, splitedMoney.length - 1).join(',')}<span class='hundred-won'>,${
+        splitedMoney[splitedMoney.length - 1]
+      }<span>
+          </span>
+      `,
     });
   };
 }
