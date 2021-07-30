de3458ae363614f3a7ab758eb24fdbe712333ddd
jihoMoon <sun_days@naver.com>
2021-07-30 11:18:50 +0900

    [#2] chore : 내역페이지 이미지 추가

--- client/src/static/imageUrls.ts
+++ client/src/static/imageUrls.ts
@@ -8,3 +8,13 @@ export const StatisticActive = BASE_URL + 'statistic_active.svg';
 export const Calendar = BASE_URL + 'calendar.svg';
 export const CalendarActive = BASE_URL + 'calendar_active.svg';
 export const Logout = BASE_URL + 'logout.svg';
+
+export const Health = BASE_URL + 'health.svg';
+export const Food = BASE_URL + 'food.svg';
+export const Transport = BASE_URL + 'transport.svg';
+export const Culture = BASE_URL + 'culture.svg';
+export const Others = BASE_URL + 'others.svg';
+export const Shopping = BASE_URL + 'shopping.svg';
+export const DailyLife = BASE_URL + 'dailylife.svg';
+export const CheckButton = BASE_URL + 'checkbutton.svg';
+export const Option = BASE_URL + 'option.svg';
