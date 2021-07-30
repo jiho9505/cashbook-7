286f84465b3c2f4ca5424743315a431850eaaf05
jihoMoon <sun_days@naver.com>
2021-07-30 11:23:42 +0900

    [#2] feat : UI - render() 구현

--- client/src/views/account/index.ts
+++ client/src/views/account/index.ts
@@ -1,7 +1,27 @@
 import handleEvent from '@src/utils/handleEvent';
 import { $ } from '@src/utils/helper';
+import {
+  Health,
+  Transport,
+  Food,
+  Culture,
+  Others,
+  Shopping,
+  DailyLife,
+  CheckButton,
+  Option,
+} from '@src/static/imageUrls';
+import '@src/views/account/index.scss';
+import { cardType } from '@src/static/constants';
 
 export default class AccountView {
+  income: string = '1,822,470원';
+  expenditure: string = '9,500원';
+  balance: string = '363,000원';
+  payMethods: string[] = ['카카오뱅크', '신한카드', '현금', '우리카드', '롯데카드', '삼성카드', '현대카드', 'anything'];
+  // 아직 타입은 확정X
+  // price / createdAt / category / paymethod / content ( 테이블에서 이정도 추가 필요  : 상세내역 부분임 )
+
   constructor() {
     handleEvent.subscribe('storeupdated', (e: CustomEvent) => {
       if (e.detail.state.path !== '/account') return;
@@ -10,7 +30,16 @@ export default class AccountView {
     });
   }
 
-  render() {
-    $('.content-wrap').innerHTML = `<h1>Account!!!</h1>`;
+  render(): void {
+    $('.content-wrap').innerHTML = `
+      <div class='account'>
+        ${this.createMyBalance()}
+        ${this.createPayMethod()}
+
+        <div class='history'>
+          ${this.createHistoryHeader()}
+          ${this.createHistoryContent()}   
+        </div>
+      </div>`;
   }
 }
