cea4711be82b6108b9cc2072410936eb6d45b029
jihoMoon <sun_days@naver.com>
2021-07-30 22:07:36 +0900

    [#2] refactor : 잔고 컴포넌트 분리

---
+++ client/src/views/page-components/account/myBalance/myBalance.ts
@@ -0,0 +1,31 @@
+import { $ } from '@src/utils/helper';
+import { createDOMWithSelector } from '@src/utils/helper';
+import './myBalance.scss';
+
+export default class MyBalance {
+  state: string;
+
+  constructor({ parent, state }) {
+    const balanceWrapper = createDOMWithSelector('div', '.account__balance-container');
+
+    parent.appendChild(balanceWrapper);
+    this.setProperty(state);
+    this.render();
+  }
+
+  setProperty(state): void {
+    this.state = state;
+  }
+
+  render(): void {
+    $('.account__balance-container').innerHTML = `
+        ${this.createMyBalance()}`;
+  }
+
+  createMyBalance(): string {
+    return `
+        <span class='account__balance-text'>현재 잔고</span>
+        <span class='account__balance'>${this.state}</span> 
+    `;
+  }
+}
