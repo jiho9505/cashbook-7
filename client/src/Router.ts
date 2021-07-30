94ffead8e35c521d21243336a20db328885f3ebc
jihoMoon <sun_days@naver.com>
2021-07-30 22:05:09 +0900

    [#2] refactor : router 경로 수정

--- client/src/Router.ts
+++ client/src/Router.ts
@@ -1,6 +1,6 @@
 import HomeView from '@src/views/home';
 import handleEvent from './utils/handleEvent';
-import AccountView from './views/account';
+import AccountView from './views/page-components/account';
 import CalendarView from './views/calendar';
 import HeaderView from './views/header';
 import StatisticsView from './views/statistics';
