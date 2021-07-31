319102ce5d4940a4fe616974d69c207889785a8f
jonghopark95 <jong951005@gmail.com>
2021-08-01 00:38:27 +0900

    feat : doughnut chart 구현

--- client/src/views/page-components/statistics-page/ExpenseByAllCategory/ExpenseByAllCategory.ts
+++ client/src/views/page-components/statistics-page/ExpenseByAllCategory/ExpenseByAllCategory.ts
@@ -9,14 +9,61 @@ export default class ExpenseByAllCategory {
     this.$ExpenseByAllCategory = createDOMWithSelector('div', '.expense-by-all-category');
     parent.appendChild(this.$ExpenseByAllCategory);
     this.render();
+    console.log(this.getCoordinatesForPercent(0.12));
   }
 
   render() {
     this.$ExpenseByAllCategory.innerHTML = `
         <span class='expense-by-all-category__title'>카테고리 별 지출</span>
-        <div class='expense-by-all-category__content'></div>
-        <div class='expense-by-all-category__sort'></div>
-    `;
+        <div class='expense-by-all-category__content'>
+          <div class='content__chart'>
+            <svg viewBox='-1.5 -1.5 3 3'>
+            ${this.getDoughnutChartPath()}
+            </svg>
+          </div>
+          <div class='content__summary'></div>
+        <div>
+        `;
+  }
+
+  getCoordinatesForPercent(percent: number): number[] {
+    const x = Math.cos(2 * Math.PI * percent);
+    const y = Math.sin(2 * Math.PI * percent);
+
+    return [x, y];
+  }
+
+  getDoughnutChartPath() {
+    const test = [
+      { percent: 0.2, color: 'Coral' },
+      { percent: 0.3, color: 'Black' },
+      { percent: 0.2, color: 'Blue' },
+      { percent: 0.15, color: 'Purple' },
+    ];
+
+    let accumulatedPercent = 0;
+    const paths = test
+      .map(({ percent, color }, i) => {
+        const [startX, startY] = this.getCoordinatesForPercent(accumulatedPercent);
+        accumulatedPercent += percent;
+        const [endX, endY] = this.getCoordinatesForPercent(accumulatedPercent);
+
+        const isLargeArcFlag = percent > 0.5 ? 1 : 0;
+
+        const targetRad = 2 * Math.PI * percent;
+        const targetRestRad = 2 * Math.PI * (1 - percent);
+
+        return `
+        <path d='M ${startX} ${startY} A 1 1 0 ${isLargeArcFlag} 1 ${endX} ${endY} L 0 0' fill=none stroke-dasharray='${targetRad} ${targetRestRad}' stroke-width=0.4 stroke-dashoffset=${targetRad} stroke=${color}>
+          <animate attributeName='stroke-dashoffset' from=${targetRad} to='0.025' dur='0.25' begin=${
+          0.25 * i
+        } fill="freeze"/>
+        </path>
+      `;
+      })
+      .join('');
+
+    return paths;
   }
 }
 
