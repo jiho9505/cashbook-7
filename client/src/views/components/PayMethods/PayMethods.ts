fec3604207798803fadfc8fe7aad2cc9ffa4a584
jihoMoon <sun_days@naver.com>
2021-07-31 17:35:02 +0900

    [#17] feat : payMethod 공통컴포넌트용으로 수정

--- client/src/views/components/PayMethods/PayMethods.ts
+++ client/src/views/components/PayMethods/PayMethods.ts
@@ -2,17 +2,26 @@ import { cardType } from '@src/static/constants';
 import handleEvent from '@src/utils/handleEvent';
 import { CheckButton, Xbox } from '@src/static/imageUrls';
 import './PayMethods.scss';
+import { $ } from '@src/utils/helper';
 
 export default class PayMethod {
   state: any;
   PayWrapper: HTMLElement;
+  mode: string = 'Account';
 
   constructor({ parent, state }) {
+    if (parent === $('.history-form__pay-method')) this.mode = 'historyModal';
+
     this.PayWrapper = document.createElement('div');
 
     parent.appendChild(this.PayWrapper);
     this.setState(state);
     this.render();
+
+    this.PayWrapper.addEventListener('click', () => {
+      if (this.mode === 'historyModal') return;
+      handleEvent.fire('createhistorymodal', {});
+    });
   }
 
   setState(state): void {
@@ -25,12 +34,20 @@ export default class PayMethod {
         `;
   }
 
+  /**
+    Modal에서 나온 부분인지 Account 페이지에서 나온 부분인지 판단
+   */
+  isHistoryModal(): boolean {
+    if (this.mode === 'historyModal') return true;
+    return false;
+  }
+
   createPayMethod(): string {
     return `
       <div>
         <div class='pay-container'>
           <span class='pay-text'>결제수단</span>
-          <span class='pay'>+</span>
+          ${this.isHistoryModal() ? `` : `<span class='pay'>+</span>`}
         </div>
         
         <div class='card-container'>
@@ -45,14 +62,14 @@ export default class PayMethod {
         return `
             <div class='card ${cardType[pay.payMethodName]}'>
               <div class='card-price'>
-                ${pay.payMethodMoney}
+              ${this.isHistoryModal() ? `` : `${pay.payMethodMoney}`}
               </div>
 
               <div class='card-check active'>
                 <img src=${CheckButton}>
               </div>
               <div class='card-option'>
-                <img src=${Xbox}>
+              ${this.isHistoryModal() ? `` : `<img src=${Xbox}>`}
               </div>
               <div class='card-name'>
                 ${pay.payMethodName}
