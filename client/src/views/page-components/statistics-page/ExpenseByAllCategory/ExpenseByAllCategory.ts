bd7a2bda00fece121c026ab1a52c635b38361714
jonghopark95 <jong951005@gmail.com>
2021-08-01 10:20:06 +0900

    refactor : 컴포넌트 별로 지출 카테고리 분리

--- client/src/views/page-components/statistics-page/ExpenseByAllCategory/ExpenseByAllCategory.ts
+++ client/src/views/page-components/statistics-page/ExpenseByAllCategory/ExpenseByAllCategory.ts
@@ -1,15 +1,19 @@
+import { COLORS_BY_CATEGORY } from '@src/static/constants';
+import { CategoryStatisticData } from '@src/types';
 import { createDOMWithSelector } from '@src/utils/helper';
 
 import './ExpenseByAllCategory.scss';
 
 export default class ExpenseByAllCategory {
   $ExpenseByAllCategory: HTMLElement;
+  $CategoryStatisticData: CategoryStatisticData[];
 
-  constructor({ parent }) {
+  constructor({ parent, state }) {
     this.$ExpenseByAllCategory = createDOMWithSelector('div', '.expense-by-all-category');
+    this.$CategoryStatisticData = state;
+
     parent.appendChild(this.$ExpenseByAllCategory);
     this.render();
-    console.log(this.getCoordinatesForPercent(0.12));
   }
 
   render() {
@@ -18,32 +22,20 @@ export default class ExpenseByAllCategory {
         <div class='expense-by-all-category__content'>
           <div class='content__chart'>
             <svg viewBox='-1.5 -1.5 3 3'>
-            ${this.getDoughnutChartPath()}
+            ${this.getDoughnutChartPath(this.$CategoryStatisticData)}
             </svg>
           </div>
-          <div class='content__summary'></div>
+          <div class='content__summary'>
+          
+          </div>
         <div>
-        `;
+      `;
   }
 
-  getCoordinatesForPercent(percent: number): number[] {
-    const x = Math.cos(2 * Math.PI * percent);
-    const y = Math.sin(2 * Math.PI * percent);
-
-    return [x, y];
-  }
-
-  getDoughnutChartPath() {
-    const test = [
-      { percent: 0.2, color: 'Coral' },
-      { percent: 0.3, color: 'Black' },
-      { percent: 0.2, color: 'Blue' },
-      { percent: 0.15, color: 'Purple' },
-    ];
-
+  getDoughnutChartPath(data: CategoryStatisticData[]) {
     let accumulatedPercent = 0;
-    const paths = test
-      .map(({ percent, color }, i) => {
+    const paths = data
+      .map(({ percent, category }, i) => {
         const [startX, startY] = this.getCoordinatesForPercent(accumulatedPercent);
         accumulatedPercent += percent;
         const [endX, endY] = this.getCoordinatesForPercent(accumulatedPercent);
@@ -54,9 +46,11 @@ export default class ExpenseByAllCategory {
         const targetRestRad = 2 * Math.PI * (1 - percent);
 
         return `
-        <path d='M ${startX} ${startY} A 1 1 0 ${isLargeArcFlag} 1 ${endX} ${endY} L 0 0' fill=none stroke-dasharray='${targetRad} ${targetRestRad}' stroke-width=0.4 stroke-dashoffset=${targetRad} stroke=${color}>
-          <animate attributeName='stroke-dashoffset' from=${targetRad} to='0.025' dur='0.25' begin=${
-          0.25 * i
+        <path d='M ${startX} ${startY} A 1 1 0 ${isLargeArcFlag} 1 ${endX} ${endY} L 0 0' fill=none stroke-dasharray='${targetRad} ${targetRestRad}' stroke-width=0.4 stroke-dashoffset=${targetRad} stroke=${
+          COLORS_BY_CATEGORY[category]
+        }>
+          <animate attributeName='stroke-dashoffset' from=${targetRad} to='0.025' dur='0.2' begin=${
+          0.2 * i
         } fill="freeze"/>
         </path>
       `;
@@ -65,42 +59,11 @@ export default class ExpenseByAllCategory {
 
     return paths;
   }
+
+  getCoordinatesForPercent(percent: number): number[] {
+    const x = Math.cos(2 * Math.PI * percent);
+    const y = Math.sin(2 * Math.PI * percent);
+
+    return [x, y];
+  }
 }
-
-// DoughnutChart = () => {
-//   return Container({
-//     tagName: 'svg',
-//     className: 'doughnut-chart',
-//     attribute: [['viewBox', '0 0 100 100']],
-//     child: [...Array(360).keys()] //
-//       .map((i) => i + 1)
-//       .map((degree) => this.DoughnutChartPath(degree)),
-//   });
-// };
-
-// DoughnutChartPath = (degree) => {
-//   const d = this.getArc({ x: 50, y: 50, radius: 40, degree });
-//   return `<path d='${d}' stroke='#5D5FEF' fill='transparent' class='hide'></path>`;
-// };
-
-// MAX_DEGREE = 359;
-// getArc = (props) => {
-//   const startCoord = this.getCoordsOnCircle({ ...props, degree: 0 });
-//   const finishCoord = this.getCoordsOnCircle({ ...props });
-
-//   const { x, y, radius, degree } = props;
-//   const isLargeArc = degree > 180 ? 1 : 0;
-//   const isEnd = degree === this.MAX_DEGREE;
-//   const d = `M ${startCoord.x} ${startCoord.y} A ${radius} ${radius} 0 ${isLargeArc} 1 ${finishCoord.x} ${
-//     finishCoord.y
-//   } L ${x} ${y} ${isEnd ? 'z' : ''}`;
-//   return d;
-// };
-
-// getCoordsOnCircle = ({ x, y, radius, degree }) => {
-//   const radian = (degree / 180) * Math.PI;
-//   return {
-//     x: x + radius * Math.cos(radian),
-//     y: y + radius * Math.sin(radian),
-//   };
-// };
