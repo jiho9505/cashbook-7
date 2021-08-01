19c1adf5c750dd7eeea1f2cecfc5e4406fc3c6ac
jonghopark95 <jong951005@gmail.com>
2021-08-02 00:44:25 +0900

    refactor : 최근 가계부 내역 로직 리팩토링

--- client/src/views/page-components/statistics-page/RecentlyAccountHistory/RecentlyAccountHistory.ts
+++ client/src/views/page-components/statistics-page/RecentlyAccountHistory/RecentlyAccountHistory.ts
@@ -1,6 +1,5 @@
 import { PICTOGRAM } from '@src/static/constants';
 import { AccountData } from '@src/types';
-import handleEvent from '@src/utils/handleEvent';
 import { changeIntoDateFormat, createDOMWithSelector } from '@src/utils/helper';
 
 import './RecentlyAccountHistory.scss';
@@ -22,32 +21,48 @@ export default class RecentlyAccountHistory {
         <span class='recently-account-history__title'>최근 가계부 내역</span>
         <a href='/account' class='recently-account-history__detail'>자세히 보기 ></a>
         <div class='recently-account-wrap'>
-          ${this.getRecentlyAccountDOM()}
+          ${this.getRecentlyAccountsDOM(this.recentlyAccountHistoryList)}
         </div>
     `;
   }
 
-  getRecentlyAccountDOM = () => {
-    return this.recentlyAccountHistoryList.map((d) => this.RecentlyAccount(d)).join('');
-  };
+  /**
+   * 최근 가계부 내역에 해당하는 DOM들을 반환합니다.
+   */
+  getRecentlyAccountsDOM = (data: AccountData[]): string => data.map((d) => this.RecentlyAccount(d)).join('');
 
-  RecentlyAccount = (data: AccountData) => {
-    const splitedMoney = data.amountOfMoney.split(',');
+  /**
+   * RecentlyAccount 컴포넌트입니다.
+   * 추후에 components로 이동할 가능성이 있지만,
+   * 협업자와의 합의가 필요하다고 생각해 해당 위치에 선언했습니다.
+   */
+  RecentlyAccount(data: AccountData): string {
+    return `
+      <div class='recently-account'>
+        <img src=${PICTOGRAM[data.category]}>
+          <div>
+            <span class='recently-account__title'>${data.title}</span>
+            <span class='recently-account__date'>${changeIntoDateFormat(data.date)}</span>
+          </div>
+          ${this.getFormattedMoneySpan(data.amountOfMoney, data.isIncome)}
+      </div>
+    `;
+  }
+
+  /**
+   * 최근 가계부 내역 금액을 formatting 하고, (300000 => ₩ 300,000)
+   * 해당 내용을 span 내에 넣어 반환합니다.
+   */
+  getFormattedMoneySpan(money: string, isIncome: boolean): string {
+    const splitedMoney = money.split(',');
 
     return `
-        <div class='recently-account'>
-          <img src=${PICTOGRAM[data.category]}>
-              <div>
-                  <span class='recently-account__title'>${data.title}</span>
-                  <span class='recently-account__date'>${changeIntoDateFormat(data.date)}</span>
-              </div>
-              <span class='recently-account__date'>
-                  ${!data.isIncome ? '-₩' : '₩'}
-                  ${splitedMoney.splice(0, splitedMoney.length - 1).join(',')}<span class='hundred-won'>,${
+      <span class='recently-account__amount'>
+          ${isIncome ? '₩' : '-₩'}
+          ${splitedMoney.splice(0, splitedMoney.length - 1).join(',')}<span class='hundred-won'>,${
       splitedMoney[splitedMoney.length - 1]
     }<span>
-              </span>
-        </div>
+      </span>
     `;
-  };
+  }
 }
