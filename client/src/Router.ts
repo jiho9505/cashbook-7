46cccc85c022e5bad78692ca2adfbe1e9cc4b184
jihoMoon <sun_days@naver.com>
2021-08-03 15:52:33 +0900

    [#23] chown : login page 라우터 등록

--- client/src/Router.ts
+++ client/src/Router.ts
@@ -3,8 +3,12 @@ import Header from './views/components/Header';
 
 import AccountView from './views/page-components/account-page';
 import AccountHistoryModal from './views/page-components/account-page/AccountHistoryModal/AccountHistoryModal';
+<<<<<<< HEAD
 import CalendarPageView from './views/page-components/calendar-page';
 import StatisticsPageView from './views/page-components/statistics-page';
+=======
+import LoginView from './views/page-components/login-page';
+>>>>>>> e9022a6 ([#23] chown : login page 라우터 등록)
 
 export default class Router {
   constructor() {
@@ -13,6 +17,7 @@ export default class Router {
     new StatisticsPageView();
     new CalendarPageView();
     new AccountHistoryModal();
+    new LoginView();
 
     window.addEventListener('popstate', (e) => {
       if (e.state === null) return;
