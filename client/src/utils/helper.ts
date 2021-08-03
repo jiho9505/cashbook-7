02e1fade58c604a200e7f78e4365109f301d3a4e
jonghopark95 <jong951005@gmail.com>
2021-08-04 00:11:01 +0900

    chore : Month 값을 Text로 바꿔주는 객체 추가

--- client/src/utils/helper.ts
+++ client/src/utils/helper.ts
@@ -1,3 +1,18 @@
+export const monthText = {
+  '1': 'Jan',
+  '2': 'Feb',
+  '3': 'Mar',
+  '4': 'Apr',
+  '5': 'May',
+  '6': 'Jun',
+  '7': 'Jul',
+  '8': 'Aug',
+  '9': 'Sep',
+  '10': 'Oct',
+  '11': 'Nov',
+  '12': 'Dec',
+};
+
 export const $ = (
   selector: string, //
   parentNode: HTMLElement | Document = document
