82d2c3896edf68d2d1e1f0176aa11071c799b024
jihoMoon <sun_days@naver.com>
2021-08-03 15:53:09 +0900

    [#23] feat : 로그인 컴포넌트 구현

---
+++ client/src/views/page-components/login-page/index.ts
@@ -0,0 +1,56 @@
+import { GithubIcon, LoginBg, Logo } from '@src/static/image-urls';
+import handleEvent from '@src/utils/handleEvent';
+import { $, createDOMWithSelector } from '@src/utils/helper';
+
+import './index.scss';
+
+export default class LoginView {
+  loginWrapper: HTMLDivElement | HTMLElement;
+
+  constructor() {
+    handleEvent.subscribe('storeupdated', (e: CustomEvent) => {
+      if (e.detail.state.path !== '/') return;
+      this.loginWrapper = createDOMWithSelector('div', '.login');
+      $('body').innerHTML = '';
+      $('body').appendChild(this.loginWrapper);
+
+      this.render();
+    });
+  }
+
+  render(): void {
+    this.loginWrapper.innerHTML = `
+    <div class="login__logo">
+      <img src=${Logo}> 
+    </div>
+    <div class="login__left-part">
+      <img src=${LoginBg}>
+      <div class="login__guide">
+          <div>자신감 있게 돈을 관리해보세요.</div>
+      </div>
+      <div class="login__guide-detail">
+          <div>통찰력을 갖고 돈의 흐름을 통제해보세요. <br>여러 가지 도구들로 당신이 올바른 결정을 할 수 있도록 도와줍니다.</div>
+      </div>
+      <div class='triangle'></div>
+    </div>
+  
+    <div class="login__right-part">
+        <div class="login__welcome">
+          <div>Sign In<br> to <span class='highlight'>woowahan</span> ledger</div>
+        </div>
+        <div class="login__button-container">
+          <button class="login__button"> 
+              <img src=${GithubIcon}>
+              <span>Sign up with Github</span>
+          </button>
+        </div>
+    </div>
+    `;
+  }
+}
+
+/*
+
+
+
+*/
