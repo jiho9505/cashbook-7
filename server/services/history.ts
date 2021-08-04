b90c26232c329592bfbf8c8ce7e238f364f6ec9e
jihoMoon <sun_days@naver.com>
2021-08-04 15:34:22 +0900

    chore : 임시 주석 처리 (error 이유로)

--- server/services/history.ts
+++ server/services/history.ts
@@ -3,7 +3,7 @@ import { historyRepo } from '../repository/history';
 
 const getHistoryById = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const data = await historyRepo.create();
+    const data = '';
 
     return res.json({
       data,
