692b3c7467645cafb764063576896f880bc4a4ca
jonghopark95 <jong951005@gmail.com>
2021-08-04 22:30:07 +0900

    chore : Add cors setting
    Co-Authored-By: Jiho-Moon <sun_days@naver.com>

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import path from 'path';
 import router from './routes/index';
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(cors());
 require('dotenv').config();
 
 app.use(express.json());
