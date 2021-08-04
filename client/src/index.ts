46e18beb34073099efa56797d54f3f85bfdb782f
jonghopark95 <jong951005@gmail.com>
2021-08-04 21:52:53 +0900

    feat : Add logic to handle parameter of url
    Github OAuth 요청을 할 경우, URL에 토큰 파라미터가 담겨 있을 경우 분리하여 각각 JS, localstorage에 보관합니다.
    
    Co-Authored-By: Jiho-Moon <sun_days@naver.com>

--- client/src/index.ts
+++ client/src/index.ts
@@ -2,9 +2,22 @@ import '@src/styles/global.scss';
 import Model from '@src/models';
 import Router from './Router';
 import handleEvent from './utils/handleEvent';
+import { setValueOnLocalStorage } from './utils/helper';
 
 new Model();
 new Router();
+
+/**
+ * OAuth 인증을 통해 홈으로 다시 redirect 되었을 경우,
+ * query parameter에 token 정보가 있다면
+ * accessToken은 JS 내에, refreshToken은 localStorage에 보관합니다.
+ */
+if (window.location.search) {
+  const [accessToken, refreshToken] = window.location.search.split('?')[1].split('&');
+
+  handleEvent.fire('storeupdated', { state: { accessToken: accessToken.split('=')[1] } });
+  setValueOnLocalStorage('refreshToken', refreshToken.split('=')[1]);
+}
 
 handleEvent.fire(
   'statechange',
