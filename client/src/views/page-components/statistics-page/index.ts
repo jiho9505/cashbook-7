985fae2b71b3fa11141b80d7f690d16932ea2460
jonghopark95 <jong951005@gmail.com>
2021-08-06 01:34:04 +0900

    chore : expenseAllCategory, recentlyAccountData 를 계산하는 로직 추가

--- client/src/views/page-components/statistics-page/index.ts
+++ client/src/views/page-components/statistics-page/index.ts
@@ -6,6 +6,8 @@ import './index.scss';
 import RecentlyAccountHistory from './RecentlyAccountHistory/RecentlyAccountHistory';
 import ExpenseByDay from './ExpenseByDay/ExpenseByDay';
 import ExpenseByAllCategory from './ExpenseByAllCategory/ExpenseByAllCategory';
+import { api } from '@src/models/api';
+import { ServerHistoryData } from '@src/types';
 
 export default class StatisticsPageView {
   store = {
@@ -18,17 +20,68 @@ export default class StatisticsPageView {
 
   constructor() {
     handleEvent.subscribe('storeupdated', (e: CustomEvent) => {
-      if (e.detail.state.path !== '/statistics') return;
+      const { path, month, year, accessToken } = e.detail.state;
+      if (path !== '/statistics') return;
 
-      const fetchedData = this.fetchDatas();
-      this.setStore(fetchedData);
-      this.render();
+      console.log(month, year, accessToken);
+
+      const fetchCalendarDataURL = `/account-history?expenditureDay=${year}-${month.toString().padStart(2, '0')}`;
+
+      api.get(fetchCalendarDataURL, accessToken).then((res) => {
+        if (res.success) {
+          // this.currentCalendarData = res.data.accountHistory;
+
+          this.processServerDataIntoStatisticsData(res.data.accountHistory);
+          // this.render();
+        }
+      });
+      // const fetchedData = this.fetchDatas();
+
+      // this.setStore(fetchedData);
+      // this.render();
     });
   }
 
-  setStore(nextStore) {
-    this.store = { ...this.store, ...nextStore };
-    // handleEvent.fire('statisticsStoreUpdated', { state: 'all' });
+  processServerDataIntoStatisticsData(data: ServerHistoryData[]) {
+    const expenseByAllCategory = this.processDataIntoExpenseByAllCategory(data);
+    const recentlyAccountData = this.processDataIntoRecentlyAccountData(data);
+    // processDataIntoRecentlyAccountData(ㅇㅁㅅㅁ)\
+    // processDataIntoExpenseBySpecificCategory
+  }
+
+  /**
+   * 서버 데이터를 가져온 후
+   * 해당 데이터 개수를 파악하여 percentage 를 계산합니다.
+   * income 요소는 포함하지 않습니다.
+   */
+  processDataIntoExpenseByAllCategory(data: ServerHistoryData[]) {
+    const amountByCategory: { [key: string]: number } = {};
+
+    const onlyExpenditureData = data.filter(({ category }) => category.name !== 'income');
+    onlyExpenditureData.forEach(({ category }) => {
+      if (!(category.name in amountByCategory)) return (amountByCategory[category.name] = 1);
+      amountByCategory[category.name]++;
+    });
+
+    const expenseByAllCategory = Object.entries(amountByCategory).map(([category, value]) => {
+      return { category, percent: (value / onlyExpenditureData.length).toFixed(2) };
+    });
+
+    return expenseByAllCategory;
+  }
+
+  /**
+   * 서버 데이터를 가공하여
+   * 가장 최근 가계부 내역을 가져옵니다.
+   * expenditureDay 순으로 sorting 후, 최상위 3가지 요소를 가져옵니다.
+   */
+  processDataIntoRecentlyAccountData(data: ServerHistoryData[]) {
+    const MAX_RECENTLY_HISTORY_DATA_AMOUNT = 3;
+    const sortedHistoryData = data.sort(
+      (prev, next) => parseInt(next.expenditureDay.split('-')[2]) - parseInt(prev.expenditureDay.split('-')[2])
+    );
+
+    return sortedHistoryData.splice(0, MAX_RECENTLY_HISTORY_DATA_AMOUNT);
   }
 
   fetchDatas() {
