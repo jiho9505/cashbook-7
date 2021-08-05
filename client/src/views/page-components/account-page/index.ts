ec2ac8701a31290e7599b8881a46dc18ec713838
jihoMoon <sun_days@naver.com>
2021-08-05 17:55:20 +0900

    feat :내역페이지에서 넘겨줄  내역 데이터 가공

--- client/src/views/page-components/account-page/index.ts
+++ client/src/views/page-components/account-page/index.ts
@@ -8,7 +8,7 @@ import './index.scss';
 
 import AccountHistory from './AccountHistory/AccountHistory';
 import Balance from './Balance/Balance';
-import { payMethodNameList } from '@src/static/constants';
+import { categoryList, payMethodNameList } from '@src/static/constants';
 
 export default class AccountView {
   state = {
@@ -66,7 +66,7 @@ export default class AccountView {
     const sign = balanceString[0];
     const balanceWithoutSign: string = balanceString.slice(1);
 
-    const formattedBalance = this.formatPrice(balanceWithoutSign, sign);
+    const formattedBalance = this.formatPrice(balanceWithoutSign, sign, '원');
     this.state.balance = formattedBalance;
   }
 
@@ -75,10 +75,25 @@ export default class AccountView {
    */
   getTotalPriceByType(accountDatas) {
     let price = 0;
+    let income = 0;
+    let expenditure = 0;
+
     accountDatas.forEach((data) => {
-      if (data.type === 'income') price += data.price;
-      else if (data.type === 'expenditure') price -= data.price;
+      if (data.type === 'income') {
+        price += data.price;
+        income += data.price;
+      } else if (data.type === 'expenditure') {
+        price -= data.price;
+        expenditure -= data.price;
+      }
     });
+
+    const incomeString = income.toString();
+    const expenditureString = expenditure.toString();
+
+    this.state.accountHistory.income = this.formatPrice(incomeString, '', '원');
+    this.state.accountHistory.expenditure = this.formatPrice(expenditureString.slice(1), '', '원');
+
     return price;
   }
 
@@ -88,7 +103,7 @@ export default class AccountView {
    * 2. 세자리 단위로 , 를 붙혀준다.
    * 3. 마지막으로 '원' 을 붙혀준다.
    */
-  formatPrice(priceWithoutSign, sign) {
+  formatPrice(priceWithoutSign, sign, unit) {
     const moneyArray = priceWithoutSign.split('');
     const moneyLength = priceWithoutSign.length;
 
@@ -103,9 +118,13 @@ export default class AccountView {
     }
 
     const result = moneyArray.join('');
-    return sign + result + '원';
+    return sign + result + unit;
   }
 
+  /**
+   * TODO:
+   * 추후 리팩토링 대상(더 깔끔하게 짤 수 있을거 같습니다)
+   */
   makePayMethodInfo(accountDatas) {
     const keyIsPayMethodIdAndValueIsTotalPrice = {
       0: 0,
@@ -117,26 +136,25 @@ export default class AccountView {
       6: 0,
       7: 0,
     };
+
     accountDatas.forEach((data) => {
       if (data.type === 'income') keyIsPayMethodIdAndValueIsTotalPrice[data.payMethodId] += data.price;
       else if (data.type === 'expenditure') keyIsPayMethodIdAndValueIsTotalPrice[data.payMethodId] -= data.price;
     });
+
     this.setFormattedPriceOfEachPayMethodId(keyIsPayMethodIdAndValueIsTotalPrice);
 
     let priceEveryCard = [];
-
-    console.log(payMethodNameList);
     for (let key in keyIsPayMethodIdAndValueIsTotalPrice) {
       priceEveryCard.push(keyIsPayMethodIdAndValueIsTotalPrice[key]);
     }
-    console.log(priceEveryCard);
 
-    let array = [];
+    let payMethodData = [];
     payMethodNameList.forEach((payMethodName, idx) => {
-      array.push({ payMethodName, payMethodMoney: priceEveryCard[idx] });
+      payMethodData.push({ payMethodName, payMethodMoney: priceEveryCard[idx] });
     });
 
-    this.state.payMethods = array;
+    this.state.payMethods = payMethodData;
   }
 
   /**
@@ -146,10 +164,47 @@ export default class AccountView {
   setFormattedPriceOfEachPayMethodId(keyIsPayMethodIdAndValueIsTotalPrice) {
     for (let key in keyIsPayMethodIdAndValueIsTotalPrice) {
       const price: string = keyIsPayMethodIdAndValueIsTotalPrice[key].toString();
-      if (price[0] === '-') keyIsPayMethodIdAndValueIsTotalPrice[key] = this.formatPrice(price.slice(1), '-');
-      else keyIsPayMethodIdAndValueIsTotalPrice[key] = this.formatPrice(price, '');
+      if (price[0] === '-') keyIsPayMethodIdAndValueIsTotalPrice[key] = this.formatPrice(price.slice(1), '-', '원');
+      else keyIsPayMethodIdAndValueIsTotalPrice[key] = this.formatPrice(price, '', '원');
     }
   }
 
-  makeAccountHistoryInfo(accountDatas) {}
+  makeAccountHistoryInfo(accountDatas) {
+    console.log('yes', accountDatas);
+
+    let array = [];
+
+    accountDatas.forEach((data) => {
+      let price = '';
+      let date = '';
+
+      price = this.getFormattedPrice(data);
+      date = this.getFormattedDate(data);
+
+      array.push({
+        price: price,
+        createdAt: date,
+        category: categoryList[data.categoryId],
+        payMethod: payMethodNameList[data.payMethodId],
+        content: data.historyContent,
+      });
+    });
+
+    console.log('array: ', array);
+  }
+
+  getFormattedPrice(data) {
+    if (data.type === 'income') {
+      return this.formatPrice(data.price.toString(), '₩ ', '');
+    } else if (data.type === 'expenditure') {
+      return this.formatPrice(data.price.toString().slice(1), '-₩ ', '');
+    }
+  }
+
+  getFormattedDate(data) {
+    const month = data.expenditureDay.slice(5, 7);
+    const day = data.expenditureDay.slice(8);
+
+    return `${Number(month)}월 ${day}일`;
+  }
 }
